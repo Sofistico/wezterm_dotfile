05edbb68dc376c2117c4b00fac6890b0d1e9d11b
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-08-01 15:36:40 -0300

    some changes to the config

--- wezterm.lua
+++ wezterm.lua
@@ -5,8 +5,8 @@ local mux = wezterm.mux
 -- This will hold the configuration.
 local config = {
 	font = wezterm.font_with_fallback({
-	{family = 'DejaVuSansM Nerd Font Mono', scale = 0.90},
-	{ family = "Symbols Nerd Font Mono", scale = 1.25}}),
+	{ family = 'DejaVuSansM Nerd Font Mono', scale = 0.90 },
+	{ family = "Symbols Nerd Font Mono", scale = 0.80} }),
 	hide_tab_bar_if_only_one_tab = true,
 	window_padding = {
 	  left = 0,
@@ -23,7 +23,9 @@ local config = {
 			key = 'F11',
 			action = wezterm.action.ToggleFullScreen,
 		},
-	}
+	},
+	--default_prog = { "C:/cygwin64/Cygwin" }
+	default_prog = { "pwsh" }
 }
 
 wezterm.on('gui-startup', function(cmd)
