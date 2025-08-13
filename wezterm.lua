8bb4fafbcb56fe40047dbf7e52ee95ab66225fc2
Sofistico <joaomegamem@gmail.com>
2025-08-13 14:34:47 -0300

    changing the pattern to get an url

--- wezterm.lua
+++ wezterm.lua
@@ -153,7 +153,7 @@ config.keys = {
 		action = wezterm.action({
 			QuickSelectArgs = {
 				patterns = {
-					"https?://\\S+",
+					"https?://\\S+\\b",
 				},
 				action = wezterm.action_callback(function(window, pane)
 					local url = window:get_selection_text_for_pane(pane)
